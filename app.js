05f76c68979becda625436e940d652e50904d093
Dane Barber <dane.barber@me.com>
2022-05-19 15:47:40 -0600

    Fat Boss

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const hero = {
+  level: 1,
   health: 100,
 }
 
@@ -7,18 +8,56 @@ const superHero = {
 }
 
 const boss = {
+  level: 1,
   health: 100,
+  attack: 5,
 }
+
+let multiplier = 1 + boss.level
+const gameHealth = 100
 
 function attackBoss() {
   boss.health -= 5
+  if (boss.health <= 0) {
+    boss.health = 0,
+      console.log("boss is ded! Level UP!!!"),
+      boss.level += 1,
+      boss.attack *= 2,
+      boss.health = gameHealth * boss.level
+
+  }
+  let maxHealth = (gameHealth * boss.level)
+  console.log(maxHealth, 'max Health')
+  let displayHealth = (boss.health / maxHealth) * 100
+  console.log(displayHealth, "display health")
   console.log(boss.health, "boss health")
-  document.getElementById("progress-boss").style.width = boss.health.toString() + "%"
+  document.getElementById("progress-boss").style.width = displayHealth.toString() + "%"
 
 }
 
 function bossAttack() {
-  hero.health -= 5
-  console.log(hero.health, "hero health")
+  hero.health -= boss.attack + Math.floor((Math.random() * (5 * multiplier)))
+  if (hero.health <= 0) {
+    hero.health = 0,
+      // console.log("hero ded"),
+      boss.attack = 0
+  }
+  // console.log(hero.health, "hero health")
   document.getElementById("progress-hero").style.width = hero.health.toString() + "%"
 }
+
+function gameReset() {
+  hero.health = 100,
+    boss.health = 100,
+    hero.level = 1,
+    boss.level = 1,
+    document.getElementById("progress-hero").style.width = hero.health.toString() + "%",
+    document.getElementById("progress-boss").style.width = boss.health.toString() + "%"
+
+}
+
+
+
+
+
+setInterval(bossAttack, 3000)
