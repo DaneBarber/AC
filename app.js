8a8e9fde979c3065deeb29a9c09ab5574e518bc7
Dane Barber <dane.barber@me.com>
2022-05-19 14:17:45 -0600

    damage

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ const superHero = {
 const boss = {
   health: 100,
 }
+
+function attackBoss() {
+  boss.health -= 5
+  console.log(boss.health, "boss health")
+  document.getElementById("progress-boss").style.width = boss.health.toString() + "%"
+
+}
+
+function bossAttack() {
+  hero.health -= 5
+  console.log(hero.health, "hero health")
+  document.getElementById("progress-hero").style.width = hero.health.toString() + "%"
+}
